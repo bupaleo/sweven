d51f595a0cc284ba3f9b4374deec0b99c9e4069e
bupaleo <bupaleo.off@gmail.com>
2023-02-24 15:00:37 +0600

    header, footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,53 +3,56 @@ import module from './Footer.module.scss'
 import phone_icon from '../assets/phone_icons.png'
 import email_icon from '../assets/email_icons.png'
 import location_icon from '../assets/location_icons.png'
-
+import inst from '../assets/inst.png'
 
 function Footer(props) {
     return (
         <footer>
-            <div className={module.footer}> <p id={module.one}> Стань частью творческих людей ! </p>
+            {/* <div className={module.footer}> <p id={module.one}> Стань частью творческих людей ! </p>
             <p id={module.two}> ПРИСОЕДИНЯЙТЕСЬ К НАМ И БУДЬТЕ  В КУРСЕ О СКИДОЧНЫХ ТОВАРОВ  </p>
             <p id={module.three}> ПОДПИШИСЬ, ЧТОБЫ НЕПРОПУСТИТЬ ТАКОЙ ШАНС  </p>
-            </div>
-            <div className={module.contacts}>
-                <div> <h4>Компания </h4>
-                    <ul>
-                        <li>Новости</li>
-                        <li>Оферта</li>
-                        <li>Поставщикам</li>
-                    </ul>
-                </div>
-
-                <div> <h4>Покупателям </h4>
-                    <ul>
-                        <li>Как оформить заказ</li>
-                        <li>Способы оплаты</li>
-                        <li>Условия доставки</li>
-                        <li>Правила возврата</li>
-                        <li>Программа лояльности</li>
-                        <li>Обратная связь</li>
-                    </ul>
-                </div>
-
-                <div>  <h4>Оптовикам </h4>
-                    <ul>
-                        <li>Общая информация</li>
-                        <li>Обзоры</li>
-                        <li>Контакт-центр</li>
-                    </ul>
-                </div>
-
-                <div> <h4 className={module.h4}>Наши контакты </h4> 
-                <ul>
-                        <li id={module.phone_icon}> +996 -XXX-XXX</li> 
-                        <li id={module.email_icon}> Служба поддержки</li>
-                        <li id={module.location_icon}> ул. Иманова 71/5</li>
-                        <li> пр. Гоголя 4/1</li>
+            </div> */}
+            <div className={module.container} >
+                <div className={module.contacts}> 
+                    <div> <h4>Компания </h4>
+                        <ul>
+                            <li>Новости</li>
+                            <li>Оферта</li>
+                            <li>Поставщикам</li>
                         </ul>
-                </div>
+                    </div>
+                    <div> <h4>Покупателям </h4>
+                            <ul>
+                                <li>Как оформить заказ</li>
+                                <li>Способы оплаты</li>
+                                <li>Условия доставки</li>
+                                <li>Правила возврата</li>
+                                <li>Программа лояльности</li>
+                                <li>Обратная связь</li>
+                            </ul>
+                    </div>
 
-            </div>
+                    <div> <h4>Оптовикам </h4>
+                        <ul>
+                            <li>Общая информация</li>
+                            <li>Обзоры</li>
+                            <li>Контакт-центр</li>
+                        </ul>
+                    </div>
+
+                    <div> <h4 className={module.h4}>Наши контакты </h4> 
+                        <ul id={module.ul}>
+                            <li id={module.phone_icon}> +996 -XXX-XXX</li> 
+                            <li id={module.email_icon}> Служба поддержки</li>
+                            <li id={module.location_icon}> ул. Иманова 71/5</li>
+                            <li> пр. Гоголя 4/1</li>
+                        </ul>
+                    </div>             
+                </div> 
+
+             </div>               
+              {/* <div className={module.line}> </div>
+                <div> <img src={inst}></img> </div> */}
         </footer>
     );
 }
