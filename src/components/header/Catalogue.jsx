88a09229b4692ccdf5757c703f3f6ddd372dc3ee
bupaleo <bupaleo.off@gmail.com>
2023-02-24 13:41:16 +0600

    header, footer

--- src/components/header/Catalogue.jsx
+++ src/components/header/Catalogue.jsx
@@ -3,11 +3,38 @@ import module from './Catalogue.module.scss'
 function Catalogue(props) {
     return (
         <div className={module.catalogue}>
-            <div> ГЛИНА </div>   
-            <div> ШИТЬЕ  ПЭЧВОРК </div> 
-            <div> ВЯЗАНИЕ</div> 
-            <div> ТОВАРЫ ДЛЯ ХУДОЖНИКОВ</div> 
-            <div> КОНЦЕЛЯРСКИЕ ТОВАРЫ</div>  
+            <div className={module.title}>  
+                <ul  className={module.ul}>ГЛИНА
+                    <li> Гончарная машина   </li> 
+                    <li> Резаки , каттеры и плунжеры  </li>
+                    <li> Ролики , линейки  </li>
+                    <li> Стеки и кисти </li>
+                    </ul> 
+                    </div>   
+            <div className={module.title}> 
+            <ul>ШИТЬЕ  ПЭЧВОРК 
+                    <li> Ткани </li> 
+                    <li> Нитки </li>
+                    </ul> </div> 
+            <div className={module.title}>  
+            <ul>ВЯЗАНИЕ
+                    <li> Пряжа </li> 
+                    <li> Вязальная буловка </li>
+                    <li> Спицы </li>
+                    </ul> 
+                    </div> 
+            <div className={module.title}> 
+            <ul>ТОВАРЫ ДЛЯ ХУДОЖНИКОВ
+                    <li> Мольберты </li> 
+                    <li> Кисти </li>
+                    <li> Стойки </li>
+                    </ul> 
+                    </div> 
+            <div className={module.title}>            
+                <ul>КОНЦЕЛЯРСКИЕ ТОВАРЫ  
+                    <li> Ручки </li> 
+                    <li> Карандаши </li>
+                    </ul> </div>  
         </div>
     );
 }
